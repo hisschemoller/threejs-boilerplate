b1e9673f5ede39e11c6051048cc2ee9fcde5314d
Wouter Hisschemöller <email@hisschemoller.com>
2020-03-26 10:38:44 +0100

    Module with helper classes for dat.GUI.

---
+++ src/js/guiHelpers.js
@@ -0,0 +1,96 @@
+/**
+ * Helper classes for dat.GUI used in threejsfundamentals tutorials.
+ * @see https://threejsfundamentals.org/threejs/lessons/
+ */
+
+/**
+ * @see https://threejsfundamentals.org/threejs/lessons/threejs-lights.html
+ */
+export class ColorGUIHelper {
+  constructor(object, prop) {
+    this.object = object;
+    this.prop = prop;
+  }
+  get value() {
+    return `#${this.object[this.prop].getHexString()}`;
+  }
+  set value(hexString) {
+    this.object[this.prop].set(hexString);
+  }
+}
+
+/**
+ * @see https://threejsfundamentals.org/threejs/lessons/threejs-textures.html
+ */
+export class DegRadHelper {
+  constructor(obj, prop) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+  get value() {
+    return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+  }
+  set value(v) {
+    this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+  }
+}
+
+/**
+ * @see https://threejsfundamentals.org/threejs/lessons/threejs-shadows.html
+ */
+export class DimensionGUIHelper {
+  constructor(obj, minProp, maxProp) {
+    this.obj = obj;
+    this.minProp = minProp;
+    this.maxProp = maxProp;
+  }
+  get value() {
+    return this.obj[this.maxProp] * 2;
+  }
+  set value(v) {
+    this.obj[this.maxProp] = v /  2;
+    this.obj[this.minProp] = v / -2;
+  }
+}
+
+/**
+ * @see https://threejsfundamentals.org/threejs/lessons/threejs-cameras.html
+ */
+export class MinMaxGUIHelper {
+  constructor(obj, minProp, maxProp, minDif) {
+    this.obj = obj;
+    this.minProp = minProp;
+    this.maxProp = maxProp;
+    this.minDif = minDif;
+  }
+  get min() {
+    return this.obj[this.minProp];
+  }
+  set min(v) {
+    this.obj[this.minProp] = v;
+    this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
+  }
+  get max() {
+    return this.obj[this.maxProp];
+  }
+  set max(v) {
+    this.obj[this.maxProp] = v;
+    this.min = this.min;  // this will call the min setter
+  }
+}
+
+/**
+ * @see https://threejsfundamentals.org/threejs/lessons/threejs-textures.html
+ */
+export class StringToNumberHelper {
+  constructor(obj, prop) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+  get value() {
+    return this.obj[this.prop];
+  }
+  set value(v) {
+    this.obj[this.prop] = parseFloat(v);
+  }
+}
